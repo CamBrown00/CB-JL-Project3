a1ae816905db454213fa6115261903e35c7c425a
cameroo52 <wifitofu@gmail.com>
2020-02-10 13:34:11 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CB_JL_Project3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CB_JL_Project3.exe"
+  "CB_JL_Project3.exe.manifest"
+  "CB_JL_Project3.pdb"
+  "CMakeFiles/CB_JL_Project3.dir/main.cpp.obj"
+  "libCB_JL_Project3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CB_JL_Project3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CB_JL_Project3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CB_JL_Project3.
+# This may be replaced when dependencies are built.
