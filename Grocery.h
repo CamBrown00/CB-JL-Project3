8d165cbf11f7522efef5e4d988b85bdba6dd56e3
Hien Lai <hientlai@gmail.com>
2020-02-21 00:08:36 -0500

    Created and implemented Grocery.h and Grocery.cpp methods

---
+++ Grocery.h
@@ -0,0 +1,19 @@
+#ifndef CB_JL_PROJECT3_GROCERY_H
+#define CB_JL_PROJECT3_GROCERY_H
+class Grocery: public Store {
+private:
+    //By default there is no price modification
+    double priceDifference = 1;
+public:
+    //Constructors
+    Grocery();
+    Grocery(vector<Isle> isles);
+
+    //Non-trivial methods
+    void logBusinessDay();
+
+    //Getters and setters
+    double getPriceDifference();
+    void setPriceDifference(double diff);
+};
+#endif //CB_JL_PROJECT3_GROCERY_H
